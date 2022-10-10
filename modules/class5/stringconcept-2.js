76246d7d486eaeb4c76e7c916f0d37866d7394bc
Dmansub <dorvan2@gmail.com>
2022-10-09 23:12:15 -0400

    homework2

---
+++ modules/class5/stringconcept-2.js
@@ -0,0 +1,27 @@
+
+/**
+ * Due date : Oct-9 (Sun) eod
+ */
+
+ const sentence1 = 'Hello Dear, how are you doing?';
+ let result1 = 0;
+ let sentence1 > 10
+  */
+ console.log(`\n\nresult1 -> ${result1}`) 15 : 10
+ 
+ const sentence2 = 'Pages That you vIeW in tHIs WinDOw wont apPeAr in the broWSer history AND thEY wont LEaVE other TRACes';
+ /**
+  * replace all instances of a or A with 'Alpha', print the result in console.
+  */
+ 
+ const sentence3 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
+ /**
+  * print the result in console:
+  * 1. the length of sentence-3
+  * 2. does sentence-3 starts with 'HeaLTh' (ignore cases)
+  * 3. does sentence-3 contains with 'Body' (ignore cases)
+  * 4. index of 'BoDy' in sentence3  (ignore cases)
+  * 5. the last-character in sentence-3
+  * 6. word 'BoDy' is present only once. (true or false)
+  */
+ 
