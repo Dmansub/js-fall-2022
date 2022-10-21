301d215943f6673e2de4aaeffcf9bf983a9ac160
Dmansub <dorvan2@gmail.com>
2022-10-21 12:44:54 -0400

    lab3

---
+++ modules/lab3/lab3homework.js
@@ -0,0 +1,47 @@
+/*
+Create a weather app
+ - Get the weather type of the day eg. Sunny, Rainy, Cloudy, Windy etc
+ - Store weather type into an array for next 5 days
+ - Based on the weather type print "Show Sun Icon", "Show Rain Icon", "Show Cloud Icon", "Show Wind Icon" etc
+ - Continue to print forecast for next 5 days
+ 
+ *Output:
+ * Day 1 -> Show sun icon
+ * Day 2 -> Rain icon
+ * Day 3 -> cloud icon
+ * Day 4 -> wind icon
+ * Day 5 -> cloud icon
+ * */
+/* for(let i = 0; i < days.length; i++){
+    console.log(days)
+ }
+*/
+
+
+let conditions = "day3";
+ 
+ switch(conditions){
+       case "day1":
+            console.log(" sun")
+               break;       
+            case "day2": 
+            console.log(" rain")   
+            break;
+            case "day3": 
+            console.log(" snow")   
+            break;
+            case "day4": 
+            console.log(" cloud")   
+            break;
+            case "day5": 
+            console.log(" clear")   
+             break;
+                     
+
+             
+
+     }
+
+     
+
+
